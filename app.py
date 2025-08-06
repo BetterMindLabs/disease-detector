cf2ddc7e3c93a88e01f5e50df4a5f5593c26c766
nishaimmadisetty <inisha1950@gmail.com>
2025-08-06 12:33:03 -0400

    Update app.py

--- app.py
+++ app.py
@@ -15,8 +15,7 @@ from sklearn.metrics import accuracy_score, classification_report, roc_auc_score
 
 from sklearn.ensemble import RandomForestClassifier
 
-df = pd.read_csv('https://raw.githubusercontent.com/nishaimmadisetty/disease-detector/refs/heads/main/Disease_symptom_and_patient_profile_dataset.csv', engine='python')
-
+df = pd.read_excel('Disease_symptom_and_patient_profile_dataset.xlsx', engine='openpyxl')
 
 features = ['Fever', 'Cough', 'Difficulty Breathing', 'Age', 'Gender', 'Blood Pressure', 'Cholesterol Level']
 target = 'Disease'
@@ -72,6 +71,7 @@ and what you need to do if you have it.These are the symptom{ Arjun }
 text1 = model.generate_content([prompt])
 
 print(text1.text)
+
 
 
 
